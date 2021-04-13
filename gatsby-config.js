9acc8ce2664c68e457e967860b9159f9fc463bdf
Ben Adam <ben.adam@isobar.com>
2021-04-13 15:40:53 +1000

    add and configure images: not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,25 @@ module.exports = {
     siteMetadata,
     plugins: [
         `gatsby-plugin-theme-ui`,
-        `gatsby-plugin-mdx`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                gatsbyRemarkplugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 640,
+                            linkImagesToOriginal: false
+                        }
+                    }
+                ]
+            }
+        },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/content`,
+                path: `${__dirname}/content/`,
                 name: `content`
             }
         }
